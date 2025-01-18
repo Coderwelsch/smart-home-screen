350921775a94b32ad1b9eb4a2a7058408bc16e95
Joseph Ribbe <social@coderwelsch.com>
2025-01-18 12:29:06 +0100

    added IS_DEV variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
+export const IS_DEV = process.env.NODE_ENV === "development"
+
 export const TODOIST_API_KEY = process.env.REACT_APP_TODOIST_API_KEY || ""
 
 export const CALENDAR_WEBCAL_URLS = (process.env.REACT_APP_CALENDAR_WEBCAL_URLS || "").split(",").map((url) => url.trim())
