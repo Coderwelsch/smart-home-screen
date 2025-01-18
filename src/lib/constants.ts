ea9ab721916b63ad01f1214869d15fa3d95eae41
Joseph Ribbe <social@coderwelsch.com>
2025-01-18 14:50:31 +0100

    added PROXY_SERVER_URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 export const IS_DEV = process.env.NODE_ENV === "development"
 
+export const PROXY_SERVER_URL = `http://localhost:${process.env.REACT_APP_PROXY_SERVER_PORT || 3001}`
+
 export const TODOIST_API_KEY = process.env.REACT_APP_TODOIST_API_KEY || ""
 
 export const CALENDAR_WEBCAL_URLS = (process.env.REACT_APP_CALENDAR_WEBCAL_URLS || "").split(",").map((url) => url.trim())
