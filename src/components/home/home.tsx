ab405c5037755bca161c504ead783cab6772ca9d
Joseph Ribbe <social@coderwelsch.com>
2025-01-19 12:34:57 +0100

    improved styles

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,17 +2,17 @@ import * as React from "react"
 import { DashboardBody } from "@/components/dashboard/body"
 import { Dashboard } from "@/components/dashboard/dashboard"
 
-
-interface HomeProps {
-}
-
-
-export const Home = (props: HomeProps) => {
+export const Home = () => {
 	return (
 		<Dashboard>
 			<DashboardBody>
-				<h1 className="font-bold text-2xl text-gray-200 mb-4">Welcome</h1>
-				<p className="text-gray-400">Welcome to the dashboard. Select a page from the navigation to get started.</p>
+				<h1 className="mb-4 text-2xl font-bold text-gray-200">
+					Welcome
+				</h1>
+				<p className="text-gray-400">
+					Welcome to the dashboard. Select a page from the navigation
+					to get started.
+				</p>
 			</DashboardBody>
 		</Dashboard>
 	)
