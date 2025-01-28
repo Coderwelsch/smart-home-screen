82fe0a4ff9be3625232b22a060561786620334d8
Joseph Ribbe <social@coderwelsch.com>
2025-01-28 23:26:54 +0100

    fixed a bug when home is disabled and no page were found when accessing /

--- src/lib/routes.tsx
+++ src/lib/routes.tsx
@@ -3,12 +3,29 @@ import { Home } from "@/features/home/home"
 import { Todoist } from "@/features/todoist/todoist"
 import { DISABLE_HOME_PAGE } from "@/lib/constants"
 import * as React from "react"
-import { createBrowserRouter, RouteObject } from "react-router-dom"
+import { FC, useEffect } from "react"
+import { createBrowserRouter, RouteObject, useNavigate } from "react-router-dom"
 
 const routes: RouteObject[] = []
 
+const RedirectRoute: FC = () => {
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (routes.length >= 2) {
+			navigate(routes[1].path as string)
+		} else {
+			throw new Error("No more routes defined")
+		}
+	}, [])
+
+	return null
+}
+
 if (!DISABLE_HOME_PAGE) {
 	routes.push({ path: "/", element: <Home />, index: true })
+} else {
+	routes.push({ path: "/", element: <RedirectRoute />, index: true })
 }
 
 if (process.env.REACT_APP_TODOIST_API_KEY) {
