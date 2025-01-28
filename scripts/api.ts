480e29b5e4faa485e116e40ae89402e705f5c757
Joseph Ribbe <social@coderwelsch.com>
2025-01-28 19:29:50 +0100

    changed REACT_APP_PROXY_SERVER_PORT to REACT_APP_API_PORT

--- scripts/api.ts
+++ scripts/api.ts
@@ -1,8 +1,8 @@
+import dotenv from "dotenv"
 import http, { IncomingMessage, ServerResponse } from "http"
 import path from "node:path"
-import url from "url"
-import dotenv from "dotenv"
 import { URL } from "node:url"
+import url from "url"
 import { getLatestCalendarData } from "./calendar"
 
 const __dirname = new URL(".", import.meta.url).pathname
@@ -10,7 +10,7 @@ const __dirname = new URL(".", import.meta.url).pathname
 dotenv.config({ path: path.resolve(__dirname, "../.env") })
 
 // eslint-disable-next-line no-undef
-const PORT = process.env.REACT_APP_PROXY_SERVER_PORT || 3001
+const PORT = process.env.REACT_APP_API_PORT || 3001
 // eslint-disable-next-line no-undef
 
 const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
--- src/features/calendar/hooks/use-get-calendar.ts
+++ src/features/calendar/hooks/use-get-calendar.ts
@@ -1,12 +1,12 @@
 import { CalEvent } from "@/features/calendar/types"
-import { PROXY_SERVER_URL } from "@/lib/constants"
+import { API_URL } from "@/lib/constants"
 import { useQuery } from "@tanstack/react-query"
 
 export const useGetCalendar = () => {
 	return useQuery<CalEvent[]>({
 		queryKey: ["calendar"],
 		queryFn: async () => {
-			const calData = await fetch(`${PROXY_SERVER_URL}/calendar`).then(
+			const calData = await fetch(`${API_URL}/calendar`).then(
 				(response) => response.json(),
 			)
 
