6ede97b843cb52f1d048e5dfc9a4ba8e8a4fbcbf
Joseph Ribbe <social@coderwelsch.com>
2025-07-10 01:24:55 +0200

    added error handling

--- src/features/calendar/hooks/use-get-calendar.ts
+++ src/features/calendar/hooks/use-get-calendar.ts
@@ -4,16 +4,24 @@ import { useQuery } from "@tanstack/react-query"
 
 export const useGetCalendar = () => {
 	return useQuery<CalEvent[]>({
+		initialData: [],
 		queryKey: ["calendar"],
 		queryFn: async () => {
 			const { protocol, hostname } = window.location
 
 			const calData = await fetch(
 				`${protocol}//${hostname}:${API_PORT}/calendar`,
-			).then((response) => response.json())
+			)
 
-			return calData
+			if (!calData.ok) {
+				throw new Error(
+					`Failed to fetch calendar data: ${calData.statusText}`,
+				)
+			}
+
+			return calData.json()
 		},
+		retry: false,
 		refetchInterval: 1000 * 60 * 5, // 5 minutes
 	})
 }
